3bc4ef8c317d8bab838deacaa92f55935a0eb342
Armend Tahiraga <armend_tahiraga@outlook.com>
2023-12-02 02:33:23 +0100

    Made the nav responsive and added framer-motion

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -2,9 +2,10 @@ import { useContext } from "react";
 import { Context } from "../Context/Context";
 import { Link } from "react-router-dom";
 import logo from "../../Images/logo.png";
+import { motion } from "framer-motion";
 
 const Nav = () => {
-	const { language, changeLanguage } = useContext(Context);
+	const { language, changeLanguage, isMenuActive, handleMenuClick } = useContext(Context);
 
 	return (
 		<nav className="absolute z-10 w-full">
@@ -12,25 +13,88 @@ const Nav = () => {
 				<Link to="/">
 					<img src={logo} alt="" className="h-20" />
 				</Link>
-				<div className="flex justify-evenly items-center">
-					<Link to="/" className="me-11 font-poppins font-medium text-primary hover:text-secondary transition-colors">
+				<div className="hidden md:block">
+					<div className="flex justify-evenly items-center">
+						<Link
+							to="/"
+							className="me-11 font-poppins font-medium text-primary hover:text-secondary transition-colors"
+						>
+							{language === "EN" ? "Home" : "Shtëpi"}
+						</Link>
+						<Link
+							to="/about"
+							className="me-11 font-poppins font-medium text-primary hover:text-secondary transition-colors"
+						>
+							{language === "EN" ? "About" : "Rreth"}
+						</Link>
+						<Link
+							to="/products"
+							className="me-11 font-poppins font-medium text-primary hover:text-secondary transition-colors"
+						>
+							{language === "EN" ? "Products" : "Produktet"}
+						</Link>
+						<Link
+							to="/contact"
+							className="me-11 font-poppins font-medium text-primary hover:text-secondary transition-colors"
+						>
+							{language === "EN" ? "Contact" : "Kontakto"}
+						</Link>
+						<p
+							className="font-poppins font-light cursor-pointer text-primary hover:text-secondary transition-colors opacity-70"
+							onClick={changeLanguage}
+						>
+							{language}
+						</p>
+					</div>
+				</div>
+				<div className="block md:hidden">
+					<svg
+						className={isMenuActive ? "ham hamRotate active" : "ham hamRotate"}
+						viewBox="0 0 100 100"
+						width="60"
+						onClick={handleMenuClick}
+					>
+						<path
+							className="line top"
+							d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"
+						/>
+						<path className="line middle" d="m 30,50 h 40" />
+						<path
+							className="line bottom"
+							d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"
+						/>
+					</svg>
+				</div>
+			</div>
+			<motion.div
+				className="md:hidden w-full"
+				variants={{
+					hidden: { height: "0px", opacity: 0 },
+					visible: { height: "152px", opacity: 1 },
+				}}
+				initial="hidden"
+				animate={isMenuActive ? "visible" : "hidden"}
+				transition={{ duration: 0.3, delay: 0.2 }}
+			>
+				<div className="flex justify-evenly items-center flex-col bg-white shadow-md py-2 w-full h-full">
+					<Link to="/" className="font-poppins font-medium text-primary hover:text-secondary transition-colors mb-1">
 						{language === "EN" ? "Home" : "Shtëpi"}
 					</Link>
 					<Link
 						to="/about"
-						className="me-11 font-poppins font-medium text-primary hover:text-secondary transition-colors"
+						className="font-poppins font-medium text-primary hover:text-secondary transition-colors mb-1"
 					>
 						{language === "EN" ? "About" : "Rreth"}
 					</Link>
 					<Link
 						to="/products"
-						className="me-11 font-poppins font-medium text-primary hover:text-secondary transition-colors"
+						className="font-poppins font-medium text-primary hover:text-secondary transition-colors mb-1"
 					>
 						{language === "EN" ? "Products" : "Produktet"}
 					</Link>
 					<Link
 						to="/contact"
-						className="me-11 font-poppins font-medium text-primary hover:text-secondary transition-colors"
+						className="font-poppins font-medium text-primary hover:text-secondary transition-colors mb-1"
 					>
 						{language === "EN" ? "Contact" : "Kontakto"}
 					</Link>
@@ -41,7 +105,7 @@ const Nav = () => {
 						{language}
 					</p>
 				</div>
-			</div>
+			</motion.div>
 		</nav>
 	);
 };
--- src/Context/Context.tsx
+++ src/Context/Context.tsx
@@ -4,18 +4,27 @@ import { ContextType } from "../Models/Models";
 export const Context = createContext<ContextType>({
 	language: "EN",
 	changeLanguage: () => {},
+	isMenuActive: false,
+	handleMenuClick: () => {},
 });
 
 const ContextProvider: React.FC<{ children: JSX.Element }> = ({ children }) => {
 	const [language, setLanguage] = useState<"EN" | "AL">("EN");
+	const [isMenuActive, setIsMenuActive] = useState<boolean>(false);
 
-	function changeLanguage() {
+	const changeLanguage = () => {
 		setLanguage((prevLanguage) => (prevLanguage === "EN" ? "AL" : "EN"));
-	}
+	};
+
+	const handleMenuClick = () => {
+		setIsMenuActive((prevIsMenuActive) => !prevIsMenuActive);
+	};
 
 	const contextValue: ContextType = {
 		language: language,
 		changeLanguage: changeLanguage,
+		isMenuActive: isMenuActive,
+		handleMenuClick: handleMenuClick,
 	};
 
 	return <Context.Provider value={contextValue}>{children}</Context.Provider>;
