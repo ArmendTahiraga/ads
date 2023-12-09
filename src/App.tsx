27e6194627ddaad2aa13595df5ee9ca3818812d2
Armend Tahiraga <armend_tahiraga@outlook.com>
2023-12-09 01:20:21 +0100

    Moved the footer from HomePage to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Nav from "./Components/Nav";
+import Footer from "./Components/Footer";
 
 const App = () => {
 	return (
 		<>
 			<Nav />
 			<Outlet />
+			<Footer />
 		</>
 	);
 };
