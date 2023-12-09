2a4645467755bd8d633325586a0d9c8c0a258a31
Armend Tahiraga <armend_tahiraga@outlook.com>
2023-12-09 14:14:08 +0100

    Changed the shadow of the video

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
@@ -16,21 +15,13 @@ export default {
 				"hero-section": "calc(100vh - 60px)",
 			},
 			boxShadow: {
-				"hero-image": "#00695Fa7 0px 0px 56px -10px",
+				"hero-image": "#00695Fa1 0px 0px 56px -10px",
 				"about-home": "#64646f4f 0px 0px 24px 0px",
 				footer: "0 4px 30px #0000002f",
 			},
 			borderColor: {
 				footer: "#ffffff4f",
 			},
-
-			/* From https://css.glass */
-			// background: rgba(255, 255, 255, 0.2);
-			// border-radius: 16px;
-			// box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-			// backdrop-filter: blur(5px);
-			// -webkit-backdrop-filter: blur(5px);
-			// border: 1px solid rgba(255, 255, 255, 0.3);
 		},
 	},
 	plugins: [],
